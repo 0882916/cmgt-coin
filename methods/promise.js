dee652136a8f84de1d80f2383af1664c546be616
Kaspar Dohrin <kdohrin@ciphix.io>
2020-02-19 09:43:10 +0100

    Remove type:module

--- methods/promise.js
+++ methods/promise.js
@@ -13,33 +13,33 @@ async function promise(last, next, url, name, n) {
         third = new Promise(function (resolve, reject) {
             setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 450))
         }),
-        // fourth = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 425))
-        // }),
-        // fifth = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 400))
-        // }),
-        // sixth = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 375))
-        // }),
-        // seventh = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 350))
-        // }),
-        // eigth = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 325))
-        // }),
-        // nineth = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 300))
-        // }),
-        // tenth = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 275))
-        // }),
-        // eleventh = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 250))
-        // }),
-        // twelveth = new Promise(function (resolve, reject) {
-        //     setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 225))
-        // }),
+        fourth = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 425))
+        }),
+        fifth = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 400))
+        }),
+        sixth = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 375))
+        }),
+        seventh = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 350))
+        }),
+        eigth = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 325))
+        }),
+        nineth = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 300))
+        }),
+        tenth = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 275))
+        }),
+        eleventh = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 250))
+        }),
+        twelveth = new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(search(url, name, compose(last, next), n * 0), 225))
+        }),
     ]
 
     Promise.race(parallel)
